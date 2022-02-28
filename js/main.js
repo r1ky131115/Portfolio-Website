37d714d9bac67eb14b4f33201bd56f941fb77839
r1ky131115 <ricardoaguirre_96@hotmail.com>
2022-02-28 20:38:43 -0300

    prueba-about

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ let btnLeft = document.querySelector(".bx-left-indent");
 let sidebar = document.querySelector(".sidebar");
 let logoContent = document.querySelector(".logo_content");
 
+let btnFlyp = document.querySelector("#flip");
+let contFlyp = document.querySelector("#cont-flyp");
+
+
 btn.onclick = () => {
     sidebar.classList.toggle("active");
 }
@@ -11,3 +15,7 @@ btn.addEventListener('click', () => {
     btn.classList.toggle('bx-menu');
     btn.classList.toggle('bx-left-indent');
 })
+
+btnFlyp.onclick = () => {
+    contFlyp.classList.toggle("active");
+}
